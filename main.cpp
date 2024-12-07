c2ceb555f8a87820252788637e137887e26693e0
Ali Saeed <asaeed5637@gmail.com>
2024-12-07 13:26:55 -0800

    Part 1 Complete

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 using namespace std;
 
+int sum_ascii(string);
 
 int main() {
-    char a = 'A';
-    cout << a << endl;
+    char a = 'a';
+    char b = 'b';
+    char c = 'c';
+    char d = 'd';
     cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
+    cout << (int) b << endl;
+    cout << (int) c << endl;
+    cout << (int) d << endl;
+
+    cout << (int) a + (int) b + (int) c + (int) d << endl;
+    cout << sum_ascii("abcd");
     
 
     return 0;
 }
 
+int sum_ascii(string str) {
+    int total = 0;
+    for (int i = 0; i < str.length(); i++) {
+        total += (int) str[i];
+    }
+    return total;
+}
 /* 
 These targets are present in the dataset and can be used for testing:
 536B9DFC93AF
